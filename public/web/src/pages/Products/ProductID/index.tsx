e411167b3df88434fcd913bdf22758b59662800a
Mateus Soares <mateussoares114@gmail.com>
2022-12-20 20:57:01 -0300

    Consumed API product by id

--- public/web/src/pages/Products/ProductID/index.tsx
+++ public/web/src/pages/Products/ProductID/index.tsx
@@ -1,6 +1,94 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import imgLoading from "F:/Programação/Desenvolvimento/projetos/wolfGames/cliente/public/web/src/assets/loading.svg";
+
+// Hooks
+import useMessage from "../../../hooks/useMessage";
+import useQuery from "../../../hooks/useQuery";
+
+// Styles
+import { Button } from "../../../styles/styles";
+import { Container, ImageProduct, Items, Description, Box } from "./styles";
+
+interface DataProductInterface {
+    id_produto: number;
+    preco_unitario: number;
+    imagem: string;
+    nome: string;
+    categoria: string;
+    serie: string;
+    quantidade_estoque: number;
+    descricao: string;
+    marca: string;
+}
+
 const ProductID = () => {
+    const [product, setProduct] = useState<Array<DataProductInterface>>();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const { msg, handleSetMessage } = useMessage();
+
+    const handleQuery = useQuery();
+
+    const { brand, id } = useParams();
+
+    useEffect(() => {
+        const getProduct = async () => {
+            const { status, data } = await handleQuery("GET", `product/${brand}/${id}`);
+
+            if(status === "success") {
+                setLoading(false);
+                setProduct(data);
+            }else if(status === "error"){
+                setLoading(false);
+                handleSetMessage(data);
+            }
+        }
+
+        getProduct();
+    }, []);
+
     return ( 
-        <h1>Product ID</h1>
+        <Container>
+            {msg && msg}
+            {!loading ? (
+                <>
+                    {product && (
+                        <>
+                            {product.map(item => (
+                                <Box key={item.id_produto}>
+                                    <ImageProduct>
+                                        <img src={`http://localhost:8080/uploads/${item.imagem}`} alt={item.nome} />
+                                    </ImageProduct>
+                                    <Items>
+                                        <h3>{item.nome}</h3>
+                                        <div className="info">
+                                            <span><strong>Código:</strong> {item.id_produto}</span>
+                                            <span><strong>Estoque:</strong> {item.quantidade_estoque}</span> 
+                                            <span><strong>Marca:</strong> {item.marca}</span> 
+                                            <span><strong>Série:</strong> {item.serie}</span> 
+                                        </div>
+                                        <div className="budge">
+                                            <span>R$ {item.preco_unitario}</span>
+                                        </div>
+                                        <div className="frete">
+                                            <span>Calcular o frete</span>
+                                            <input type="text" placeholder="CEP" />
+                                            <Button type="button">OK</Button>
+                                        </div>
+                                        <Button width="50%" type="button">Adicionar ao carrinho</Button>
+                                    </Items>
+                                    <Description>
+                                        <h3>Descrição do produto</h3>
+                                        <p>{item.descricao}</p>
+                                    </Description>
+                                </Box>
+                            ))}
+                        </>
+                    )}
+                </>
+            ) : <img className="loading" src={imgLoading} />}
+        </Container>
     );
 }
  
--- public/web/src/pages/Products/ProductID/styles.ts
+++ public/web/src/pages/Products/ProductID/styles.ts
@@ -1,1 +1,102 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 950px;
+    background-color: #fff;
+    margin: 0 auto;
+    border-radius: 5px;
+
+    .loading{
+        margin: 0 auto;
+    }
+
+    @media (max-width: 948px){
+        width: 100%;
+    }
+`
+
+export const Box = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`
+
+export const ImageProduct = styled.div`
+    width: 50%;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 5px;
+    }
+
+    @media (max-width: 640px){
+        width: 100%;
+    }
+`
+
+export const Items = styled.div`
+    padding: 1.7rem .7rem;
+    width: 50%;
+    margin-top: .8rem;
+    display: flex;
+    flex-direction: column;
+
+    h3{
+        margin-bottom: 1.5rem;
+    }
+
+    .info span{
+        display: block;
+        margin-bottom: 5px;
+        font-size: .9em;
+    }
+
+    .budge{
+        margin: 3rem 0 1.2rem 0;
+    }
+
+    .budge span{
+        font-size: 1.7em;
+        font-weight: bold;
+        color: #9466ff;
+    }
+
+    .frete{
+        margin-bottom: 1.2rem;
+    }
+
+    .frete span{
+        display: block;
+        font-size: .9em;
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    .frete button{
+        font-size: .7em;
+        margin-left: 5px;
+    }
+
+    .frete input{
+        padding: .5rem .2rem;
+    }
+
+    @media (max-width: 640px){
+        width: 100%;
+        align-items: center;
+    }
+`
+
+export const Description = styled.div`
+    width: 100%;
+    padding: 1.7rem 2rem;
+    margin-bottom: 1.5rem;
+
+    h3{
+        text-align: center;
+        margin-bottom: 1.5rem;
+    }
+`
